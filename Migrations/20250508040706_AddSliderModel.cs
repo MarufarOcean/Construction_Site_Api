4095f89ae9da5e0cd921f2b9ef36e33a68e9903c
MarufarOcean <marufarocean@gmail.com>
2025-05-08 10:13:06 +0600

    Updated DBContext

---
+++ Migrations/20250508040706_AddSliderModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_Process.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSliderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "stock",
+                table: "Products",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Products",
+                newName: "ImageUrl");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Sliders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sliders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sliders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Products",
+                newName: "stock");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Products",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
